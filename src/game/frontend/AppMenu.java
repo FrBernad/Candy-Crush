408fb8e24e773f1a0bd03830849fe14c99859108
FrBernad <franbernad@hotmail.com>
2019-12-05 16:49:30 -0300

    levels ready

--- src/game/frontend/AppMenu.java
+++ src/game/frontend/AppMenu.java
@@ -1,13 +1,13 @@
 package game.frontend;
 
+import game.backend.level.Level;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-
 import java.util.Optional;
-
 import game.backend.CandyGame;
-
 import game.backend.level.Level1;
 import game.backend.level.Level2;
 import game.backend.level.Level3;
@@ -15,47 +15,36 @@ import javafx.stage.Stage;
 
 public class AppMenu extends MenuBar {
 
+    protected Stage level;
+
+    private EventHandler<ActionEvent> provider(Class<? extends Level> levelClass)
+    {
+        return new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                CandyGame game = new CandyGame(levelClass);
+                CandyFrame frame = new CandyFrame(game);
+                Scene scene = new Scene(frame);
+                level.setResizable(false);
+                level.setScene(scene);
+                level.show();
+            }
+        };
+    }
+
     public AppMenu() {
 
         // ARCHIVO ////////////////////////////////////////////////////////////////
         Menu file = new Menu("Archivo");
-
         // CHANGE LEVEL
         Menu changeLevel = new Menu("Change level");
-
         MenuItem lvl1 = new MenuItem("New level 1");
         MenuItem lvl2 = new MenuItem("New level 2");
         MenuItem lvl3 = new MenuItem("New level 3");
 
-        lvl1.setOnAction(event -> {
-            CandyGame game = new CandyGame(Level1.class);
-            CandyFrame frame = new CandyFrame(game);
-            Scene scene = new Scene(frame);
-            Stage stage = new Stage();
-            stage.setScene(scene);
-            stage.show();
-        });
-
-        lvl2.setOnAction(event -> {
-            CandyGame game = new CandyGame(Level2.class);
-            CandyFrame frame = new CandyFrame(game);
-            Scene scene = new Scene(frame);
-            Stage stage = new Stage();
-            stage.setScene(scene);
-            stage.show();
-        });
-
-        lvl3.setOnAction(event -> {
-
-
-            CandyGame game = new CandyGame(Level3.class);
-            CandyFrame frame = new CandyFrame(game);
-            Scene scene = new Scene(frame);
-            Stage stage = new Stage();
-            stage.setScene(scene);
-            stage.show();
-
-        });
+        lvl1.setOnAction(provider(Level1.class));
+        lvl2.setOnAction(provider(Level2.class));
+        lvl3.setOnAction(provider(Level3.class));
 
         changeLevel.getItems().addAll(lvl1, lvl2, lvl3);
 
--- src/game/frontend/GameApp.java
+++ src/game/frontend/GameApp.java
@@ -1,6 +1,7 @@
 package game.frontend;
 
 import game.backend.CandyGame;
+import game.backend.level.Level;
 import game.backend.level.Level1;
 import game.backend.level.Level2;
 import game.backend.level.Level3;
@@ -16,82 +17,61 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class GameApp extends Application {
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
-	
-	@Override
-	public void start(Stage primaryStage) {
-				
-		primaryStage.setTitle("CandyCrush");
+
+    protected Stage primaryStage;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage=primaryStage;
+        primaryStage.setTitle("CandyCrush");
+
         Button level1 = new Button();
         level1.setText("Level 1");
         level1.setMinSize(70, 30);
-        level1.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-            	CandyGame game = new CandyGame(Level1.class);
-                CandyFrame frame = new CandyFrame(game);
-                Scene scene = new Scene(frame);
-                primaryStage.setResizable(false);
-                primaryStage.setScene(scene);
-                primaryStage.show();
-            
-            }
-        });
-        
+        level1.setOnAction(provider(Level1.class));
+
         Button level2 = new Button();
         level2.setText("Level 2");
         level2.setMinSize(70, 30);
-        level2.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-            	CandyGame game = new CandyGame(Level2.class);
-                CandyFrame frame = new CandyFrame(game);
-                Scene scene = new Scene(frame);
-                primaryStage.setResizable(false);
-                primaryStage.setScene(scene);
-                primaryStage.show();
-            
-            }
-        });
-        
- 
+        level2.setOnAction(provider(Level2.class));
+
         Button level3 = new Button();
         level3.setText("Level 3");
-        level3.setMinSize(70, 30);       
-        level3.setOnAction(new EventHandler<ActionEvent>() {
-          
-        	@Override
-            public void handle(ActionEvent event) {
-              
+        level3.setMinSize(70, 30);
+        level3.setOnAction(provider(Level3.class));
 
-        		CandyGame game = new CandyGame(Level3.class);
-                CandyFrame frame = new CandyFrame(game);
-                Scene scene = new Scene(frame);
-                primaryStage.setResizable(false);
-                primaryStage.setScene(scene);
-                primaryStage.show();
-
-                
-            }           
-        });
-        
         StackPane root = new StackPane();
         HBox buttons = new HBox();
-        buttons.setPadding(new Insets(70,70,70,70));
+        buttons.setPadding(new Insets(70, 70, 70, 70));
         Pane spacer1 = new Pane();
         spacer1.setMinSize(50, 1);
         Pane spacer2 = new Pane();
         spacer2.setMinSize(50, 1);
-        buttons.getChildren().addAll(level1,spacer1,level2,spacer2,level3);
+        buttons.getChildren().addAll(level1, spacer1, level2, spacer2, level3);
         root.getChildren().addAll(buttons);
         primaryStage.setScene(new Scene(root, 400, 100));
         primaryStage.show();
-			 
-	}
+
+    }
+
+    EventHandler<ActionEvent> provider(Class<? extends Level> levelClass)
+    {
+        return new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                CandyGame game = new CandyGame(levelClass);
+                CandyFrame frame = new CandyFrame(game);
+                Scene scene = new Scene(frame);
+                primaryStage.setResizable(false);
+                primaryStage.setScene(scene);
+                primaryStage.show();
+            }
+        };
+    }
 
 }
