7d9c369e09b7abc9927aa9add32ccd5459b6c4f9
lulydk <52382953+lulydk@users.noreply.github.com>
2019-12-06 13:29:16 -0300

    Sección de ayuda e íconos para el menú

--- src/game/frontend/AppMenu.java
+++ src/game/frontend/AppMenu.java
@@ -2,23 +2,27 @@ package game.frontend;
 
 import javafx.application.Platform;
 import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+
 import java.util.Optional;
 import javafx.stage.Stage;
 
 public class AppMenu extends MenuBar {
 
-    private Stage primaryStage;
-
     public AppMenu(Stage primaryStage) {
+    	
         // ARCHIVO ////////////////////////////////////////////////////////////////
-        Menu file = new Menu("Archivo");
-        // CHANGE LEVEL
-        MenuItem changeLevel = new MenuItem("Cambiar nivel");
+     
+    	Menu file = new Menu("File");
+   
+    	// CHANGE LEVEL
+        MenuItem changeLevel = new MenuItem("Change level");
+        changeLevel.setGraphic(new ImageView("images/changeLevelIcon.png"));
         changeLevel.setOnAction(e -> {
             Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-            alert.setTitle("Cambiar de nivel");
-            alert.setHeaderText("Cambiar de nivel");
-            alert.setContentText("¿Está seguro que desea salir del nivel?");
+            alert.setTitle("Change level");
+            alert.setHeaderText("Change level");
+            alert.setContentText("Are you sure you want to change the level?");
             Optional<ButtonType> result = alert.showAndWait();
             if (result.isPresent()) {
                 if (result.get() == ButtonType.OK) {
@@ -30,12 +34,13 @@ public class AppMenu extends MenuBar {
         });
 
         // EXIT
-        MenuItem exitMenuItem = new MenuItem("Salir");
+        MenuItem exitMenuItem = new MenuItem("Exit");
+        exitMenuItem.setGraphic(new ImageView("images/exitIcon.png"));
         exitMenuItem.setOnAction(event -> {
             Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-            alert.setTitle("Salir");
-            alert.setHeaderText("Salir de la aplicación");
-            alert.setContentText("¿Está seguro que desea salir de la aplicación?");
+            alert.setTitle("Exit");
+            alert.setHeaderText("Exit application");
+            alert.setContentText("Are you sure you want to exit?");
             Optional<ButtonType> result = alert.showAndWait();
             if (result.isPresent()) {
                 if (result.get() == ButtonType.OK) {
@@ -43,22 +48,39 @@ public class AppMenu extends MenuBar {
                 }
             }
         });
+   
         file.getItems().addAll(changeLevel, exitMenuItem);
 
         // AYUDA //////////////////////////////////////////////////////////////////
-        Menu help = new Menu("Ayuda");
-
-        MenuItem aboutMenuItem = new MenuItem("Acerca De");
-        aboutMenuItem.setOnAction(event -> {
+    
+        Menu help = new Menu("Help");
+        
+        // Game guide
+        MenuItem gameGuide = new MenuItem("Game guide");
+        gameGuide.setGraphic(new ImageView("images/helpIcon.png"));
+        gameGuide.setOnAction(event -> {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Acerca De");
-            alert.setHeaderText("Candy TPE");
-            alert.setContentText("Cátedra POO 2019.\n" +
-                    "Implementación Original: Laura Zabaleta (POO 2013).\n" + "Actualización: Francisco Bernad, Camila Ponce y Luciana Diaz.");
+            alert.setTitle("Game guide");
+            alert.setHeaderText("Candy Crush 2.0: TPE Guide");
+            alert.setContentText("Level 1: Basic Mode\nThe very popular version of Candy Crush we all know and love. Combine sweets until you get the final score!\n\nLevel 2: Time's Up\nTime is running! Get the maximum score before it's too late. Combine the special candies to gain more time!\n\nLevel 3: Time Bombs\nThink wisely before you play, your movements are numbered! Try to get the final score and crush the bomb candies for more opportunities.");
             alert.showAndWait();
         });
 
-        help.getItems().add(aboutMenuItem);
+        // About
+        MenuItem aboutMenuItem = new MenuItem("About CandyCrush 2.0");
+        aboutMenuItem.setGraphic(new ImageView("images/aboutIcon.png"));
+        aboutMenuItem.setOnAction(event -> {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("About");
+            alert.setHeaderText("Candy Crush 2.0: TPE Credits");
+            alert.setContentText("Cátedra POO 2019.\n" +
+                    "Implementación Original: Laura Zabaleta (POO 2013).\n" + "Actualización: Francisco Bernad, Camila Ponce y Luciana Diaz K.");
+            ImageView graphic=new ImageView("images/graphic.png");
+            alert.setGraphic(graphic);
+            alert.showAndWait();
+        });
+
+        help.getItems().addAll(gameGuide,aboutMenuItem);
 
         // GENERAL ////////////////////////////////////////////////////////////////
         getMenus().addAll(file, help);
