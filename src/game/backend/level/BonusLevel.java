7d748225cd54dd5dc7662f4a90a7fa7a056dc0f2
lulydk <52382953+lulydk@users.noreply.github.com>
2019-12-06 15:53:07 -0300

    // Comentado

--- src/game/backend/level/BonusLevel.java
+++ src/game/backend/level/BonusLevel.java
@@ -2,19 +2,23 @@ package game.backend.level;
 
 import game.backend.element.NumberedCandy;
 import game.backend.move.Move;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class BonusLevel extends Level {
 
-    protected boolean started = false;
-    protected boolean firstPass = true;
+    BonusLevel(int requiredScore) {
+		super(requiredScore);
+	}
 
+	protected boolean started = false;
+    protected boolean firstPass = true;
     protected List<NumberedCandy> specialCandies = new ArrayList<>();
 
     public abstract void update();
-
+    
+    // Sobreescribe el ya definido para actualizar en el debido momento
+    // la grilla con caramelos especiales.
     @Override
     public boolean tryMove(int i1, int j1, int i2, int j2) {
         Move move = moveMaker.getMove(i1, j1, i2, j2);
